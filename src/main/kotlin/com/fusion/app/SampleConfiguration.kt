7c661af97d7a2ae2542edeb3c637f4047fc3a7f8
Junnyjun <chbe5082@naver.com>
2024-09-23 01:11:17 +0900

    register runtime bean

---
+++ src/main/kotlin/com/fusion/app/SampleConfiguration.kt
@@ -0,0 +1,7 @@
+package com.fusion.app
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class SampleConfiguration {
+}
--- src/test/kotlin/IocContainerTest.kt
+++ src/test/kotlin/IocContainerTest.kt
@@ -2,6 +2,9 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.BeanFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
+import org.springframework.context.support.GenericApplicationContext
+import org.springframework.context.support.registerBean
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
@@ -12,8 +15,12 @@ class IocContainerTest {
     @Autowired
     lateinit var beanFactory: BeanFactory
 
+    @Autowired
+    lateinit var applicationContext:GenericApplicationContext
     @Test
     fun test() {
+        applicationContext.registerBean<Any>("test")
         beanFactory.getBean("HomeController")
     }
 }
+
